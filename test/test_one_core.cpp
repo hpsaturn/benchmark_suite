347223606be102091c887845047c89baaaa8c998
Hpsaturn <hpsaturn@gmail.com>
2022-03-06 16:18:13 +0100

    added test for palindrome numbers

--- test/test_one_core.cpp
+++ test/test_one_core.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
 #include <unity.h>
 
-float i;
-float k;
-float j;
-float k1;
-float k2;
-long init_time;
-long final_time;
 
-void test_one_core(void) {
-    init_time = millis();
+long init_time;
+long primes_time;
+long palindromes_time;
+
+void test_prime_numbers(void) {
+    float i, k, j, k1, k2;
+    long primes_init_time = millis();
     for (i = 2; i <= 10000; i++) {
         k = int(sqrt(i)) + 1;
         for (j = 2; j <= k; j++) {
@@ -24,22 +22,51 @@ void test_one_core(void) {
             // Serial.println(i);
         }
     }
-    final_time = millis() - init_time;
-    Serial.print(final_time); 
+    primes_time = millis() - primes_init_time;
+    Serial.print("TEST prime numbers\t: ");
+    Serial.print(primes_time);
     Serial.println(" miliseconds\n");
-    TEST_ASSERT_EQUAL_INT_MESSAGE(10001, i, "One core test failed");
-    Serial.println("TEST END");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(10001, i, "Prime numbers test failed");
+}
+
+void test_palindrome_numbers(void) {
+    long palindromes_init_time = millis();
+    int i, n, b, rev = 0;
+    for (i = 0; i <= 10000; i++) {
+        n = i;
+        while (n > 0) {
+            // Find reverse of n
+            b = n % 10;
+            rev = rev * 10 + b;
+            n = n / 10;
+        }
+        // If n and rev are same, n is palindrome number
+        if (rev == i) {
+            // Serial.println(i + " ");
+        }
+        rev = 0;
+    }
+
+    palindromes_time = millis() - palindromes_init_time;
+    Serial.print("TEST palindrome numbers\t: ");
+    Serial.print(palindromes_time);
+    Serial.println(" miliseconds\n");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(10001, i, "Palindrome test failed");
 }
 
 void setup() {
     delay(2000);  // test service delay
     UNITY_BEGIN();
     Serial.begin(115200);
-    Serial.println("\nTEST Board\t: "+String(FLAVOR));
-    Serial.println("TEST One Core\t: 10000 prime numbers");
+    Serial.println("\nTEST BOARD "+String(FLAVOR));
     Serial.flush();
-    Serial.print("TEST final time\t: ");
-    RUN_TEST(test_one_core);
+    init_time = millis();
+    RUN_TEST(test_prime_numbers);
+    // RUN_TEST(test_palindrome_numbers);
+    Serial.print("TEST excution time\t: ");
+    Serial.print(palindromes_time + primes_time);
+    Serial.println(" miliseconds\n");
+    Serial.println("TEST END");
     UNITY_END();
 }
 
